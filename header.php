4106785efa4435cc3c9eb3e9ef2ce6c1423bde07
Mateusz S <mateuszuk1992@gmail.com>
2020-08-11 09:34:12 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title><!-- Insert your title here --></title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<header>
+    <div class="logo"><p>logo</p></div>
+    
+            <ul class="menu-hl">
+                <li><a href="#">Something1</a></li>
+                <li><a href="#">Something2</a></li>
+                <li><a href="#">Something3</a></li>
+                <li><a href="#">Something4</a></li>
+            </ul>
+
+</header>
+    
+    </body></html>
